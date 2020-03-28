5372fb4b5dc3815a39e5b55b237938882b70c624
Raghav Jain <raghavjain3@gmail.com>
2020-03-28 02:59:15 -0700

    XCode setup

---
+++ LAHacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LAHacks
+//
+//  Created by Raghav Jain on 3/28/20.
+//  Copyright © 2020 Raghav Jain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
